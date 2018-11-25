af1ddf5db3ac5fad768ade71b7b0436b5c35ff17
Tyquan <tyquanjreddick@gmail.com>
2018-11-25 17:11:14 -0500

    added design

---
+++ Assets/Scripts/Project_Boost/Custom/LandingPad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingPad : MonoBehaviour {
+
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
